ccc2ce2e460869724ca765180dc5e993ee50e537
Kelly Bodwin <kelly@bodwin.us>
2022-03-29 11:22:08 +0000

    fixes

---
+++ installs.R
@@ -0,0 +1,5 @@
+system("python3 -m pip install jupyter")
+system("python3 -m pip install seaborn")
+system("python3 -m pip install pandas")
+system("python3 -m pip install numpy")
+system("python3 -m pip install sklearn")
